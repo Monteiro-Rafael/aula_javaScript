dabd5907062aede5e89d228f4c79602516f622b9
Monteiro-Rafael <rafaeldonmonteiro@gmail.com>
2021-02-04 19:23:13 -0300

    Aula 2

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
-var nome = "Rafael Monteiro";
-var idade = 29;
-var idade2 = 10;
-var frase = "Japão é o melhor time do mundo";
+
+// var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+// console.log(frutas.nome);
+// alert(frutas.cor);
+// var fruta = {nome: "maça", cor: "vermelha"}
+// console.log(fruta.nome);
+// alert(fruta.cor);
+
+
+
+// var lista = ["maça", "pera", "laranja"];
+// lista.push("uva");
+// lista.pop();
+// console.log(lista[1]);
+// console.log(lista.length());
+// console.log(lista.reserse());
+// console.log(lista.join(" "));
+// console.log(lista.toString()[0]);
+
+
+
+
+// var nome = "Rafael Monteiro";
+// var idade = 29;
+// var idade2 = 10;
+// var frase = "Japão é o melhor time do mundo";
 // alert(nome + " tem " + idade + " anos.");
 // alert(idade + idade2);
-console.log(nome);
-console.log(idade + idade2);
-console.log(frase.replace("Japão", "Brasil"));
-alert(frase.replace("Japão", "Brasil"));
+// console.log(nome);
+// console.log(idade + idade2);
+// console.log(frase.replace("Japão", "Brasil"));
+// alert(frase.replace("Japão", "Brasil"));
