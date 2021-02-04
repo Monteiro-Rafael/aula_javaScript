ecb175f6900bb8b41732ad13c08467b616525a1f
Monteiro-Rafael <rafaeldonmonteiro@gmail.com>
2021-02-04 19:30:14 -0300

    Aula 3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMonth()+1);
+alert(d.getFullYear());
+
+
+
+// var count;
+// for (count=0; count<=5; count++){
+//     alert(count);
+// };
+
+
+// while (count < 5){
+//     console.log(count);
+//     alert(count);
+//     count++;
+// }
+
+
+// var idade = prompt("Qual a sua idade?");
+// if (idade >= 18) {
+//     alert("maior de idade");
+// } else {
+//     alert("menor de idade");
+// }
+
+
 
 // var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
 // console.log(frutas.nome);
