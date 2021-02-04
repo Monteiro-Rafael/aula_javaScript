6bdf8c55f5f804b703035e9068f02eac20778930
Monteiro-Rafael <rafaeldonmonteiro@gmail.com>
2021-02-04 20:25:54 -0300

    aulas 4 e 5

--- js/main.js
+++ js/main.js
@@ -1,8 +1,71 @@
-var d = new Date();
-alert(d.getDay());
-alert(d.getHours());
-alert(d.getMonth()+1);
-alert(d.getFullYear());
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar!</b>";
+    //alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+    window.open("https://github.com/Monteiro-Rafael");
+    window.location.href = "https://github.com/Monteiro-Rafael";
+}
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    alert("trocar texto");
+}
+
+function voltar(){
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("pagina carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+
+}
+
+
+// function soma (n1, n2){
+//     return n1 + n2;
+// }
+
+
+// function validaIdade(idade){
+//     var validar;
+//     if (idade >= 18){
+//         validar = true;
+//     } else {
+//         validar = false;
+//     }
+    
+//     return validar;
+    
+// }
+
+// var idade = prompt("Qual a sua idade?");
+// console.log(validaIdade(idade));
+
+// function setReplace(frase, nome, novo_nome){
+//     return frase.replace(nome, novo_nome);
+// }
+//alert(soma(5, 10));
+//alert(setReplace("vai Japão", "Japão", "Brasil"));
+
+
+// var lista = ["Alemanha", "Inglaterra", "Escócia"];
+// lista.push("Polônia");
+// lista.pop("Inglaterra");
+// alert(lista);
+
+
+
+// var d = new Date();
+// alert(d.getDay());
+// alert(d.getHours());
+// alert(d.getMonth()+1);
+// alert(d.getFullYear());
 
 
 
